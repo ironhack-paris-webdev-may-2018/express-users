7b24f5df219ff55d36bd304662825b4e64cd2515
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-07-04 17:05:59 +0200

    Add file uploads

--- routes/room-router.js
+++ routes/room-router.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
 
 const Room = require("../models/room-model.js");
 
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.cloudinary_name,
+  api_key: process.env.cloudinary_key,
+  api_secret: process.env.cloudinary_secret
+});
+const storage =
+  cloudinaryStorage({
+    cloudinary,
+    folder: "room-pictures"
+  });
+const uploader = multer({ storage });
 
 
 router.get("/room/add", (req, res, next) => {
@@ -17,20 +32,31 @@ router.get("/room/add", (req, res, next) => {
   res.render("room-views/room-form.hbs");
 });
 
-router.post("/process-room", (req, res, next) => {
-  if (!req.user) {
-    // "req.flash()" is defined by the "connect-flash" package
-    req.flash("error", "You must be logged in.");
-    // redirect away if you aren't logged in (authorization!)
-    res.redirect("/login");
-    return;
-  }
+// "pictureUpload" is our file input's name attribute
+router.post("/process-room",
+  uploader.single("pictureUpload"),
+  (req, res, next) => {
+    if (!req.user) {
+      // "req.flash()" is defined by the "connect-flash" package
+      req.flash("error", "You must be logged in.");
+      // redirect away if you aren't logged in (authorization!)
+      res.redirect("/login");
+      return;
+    }
 
-  const { name, description, pictureUrl, latitude, longitude } = req.body;
-  // create the geoJSON structure for our lat and long
-  const location = { coordinates: [ latitude, longitude ] };
+    const { name, description, pictureUrl, latitude, longitude } = req.body;
+    // multer stores the file information in "req.file"
+    const { secure_url } = req.file;
+    // create the geoJSON structure for our lat and long
+    const location = { coordinates: [ latitude, longitude ] };
 
-  Room.create({ owner: req.user._id, name, description, pictureUrl, location })
+    Room.create({
+      owner: req.user._id,
+      pictureUrl: secure_url,
+      name,
+      description,
+      location,
+    })
     .then((roomDoc) => {
       // "req.flash()" is defined by the "connect-flash" package
       req.flash("success", "Room created!");
@@ -39,7 +65,7 @@ router.post("/process-room", (req, res, next) => {
     .catch((err) => {
       next(err);
     })
-});
+  });
 
 router.get("/my-rooms", (req, res, next) => {
   if (!req.user) {
